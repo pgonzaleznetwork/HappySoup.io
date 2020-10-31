5dc8f21e2c7a3c23714e7bf6fe2945ec629c11bd
pablo <pgonzaleznetwork@gmail.com>
2020-10-31 19:07:15 +0000

    reports endpoint, new pills implementation, pills for filtering, grouping and private reports

--- sfdc_apis/endpoints.js
+++ sfdc_apis/endpoints.js
@@ -4,5 +4,6 @@ let version = process.env.SFDC_API_VERSION;
 module.exports = {
     toolingApi:`/services/data/v${version}/tooling/query/?q=`,
     metadataApi:`/services/Soap/m/${version}`,
-    soapApi:`/services/Soap/c/${version}`
+    soapApi:`/services/Soap/c/${version}`,
+    reportsApi:`/services/data/v${version}/analytics/`
 }
