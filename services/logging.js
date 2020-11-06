6a1598923356488089987179ec08816bd6662f42
Pablo Gonzalez <pgonzaleznetwork@gmail.com>
2020-11-06 14:20:19 +0000

    better error logging; redesigned email template search; tooling api doesnt throw access errors (#66)

--- services/logging.js
+++ services/logging.js
@@ -1,7 +1,7 @@
 
 
 function logError(text,details){
-    console.log(`HAPPY SOUP ERROR: ${text}`,JSON.stringify(details));
+    console.log(`HAPPY SOUP ERROR: ${text}`,JSON.stringify(details,null,2));
 }
 
 module.exports = logError;
--- sfdc_apis/metadata-types/EmailTemplate.js
+++ sfdc_apis/metadata-types/EmailTemplate.js
@@ -5,80 +5,52 @@ let toolingAPI = require('../tooling');
 async function findReferences(connection,entryPoint){
 
     let references = [];
-
     let toolingApi = toolingAPI(connection);
 
-    let wfAlertQuery = createWfAlertQuery(entryPoint.id);
-    let wfAlerts = await toolingApi.query(wfAlertQuery);
-    wfAlerts = parseWfAlerts(wfAlerts);
-
-    references.push(...wfAlerts);
-
-    let apexClassQuery = createApexClassQuery();
-    let apexClasses = await toolingApi.query(apexClassQuery);
-    apexClasses = parseApexClasses(apexClasses);
-
-    references.push(...apexClasses);
-
-    //this can throw an error if the email template is in a private user's folder
-    //rather than failing completely, we just ignore this part and return the other results
-    try{
-
-        let emailTemplQuery = createEmailTemplateQuery(entryPoint.id);
-        let emailTemplate = await toolingApi.query(emailTemplQuery);
-        let templateFullName = emailTemplate.records[0].FullName;
-
-        let customLabelQuery = createCustomLabelQuery(entryPoint,templateFullName);
-        let customLabels = await toolingApi.query(customLabelQuery);
-        customLabels = parseCustomLabels(customLabels);
-        
-        references.push(...customLabels);
-
-    }catch (error) {
-        //nothing to do, not critical functionality
-    }
+    references.push(
+        ...await findWorkflowAlerts(),
+        ...await findApexClasses(),
+        ...await findCustomLabels()
+    );
 
     return references;
 
-    function parseWfAlerts(rawResults){
+    async function findCustomLabels(){
 
-        let wfAlerts = rawResults.records.map(record => {
+        let ids = utils.filterableId(entryPoint.id);
+
+        //we need the template full name to see if it's referenced in any soql queries
+        //the full name is the api name, which is not the same as the entryPoint.name
+        //because entryPoint.name comes from a bulk query on the EmailTemplate object
+        //and querying the fullName is only allowed when the query returns 1 record
+        let query = `SELECT FullName
+        FROM EmailTemplate WHERE Id IN ('${ids}')`;
     
-            let simplified = {
-                name:`${record.TableEnumOrId}.${record.DeveloperName}`,
-                type:'WorkflowAlert',
-                id: record.Id,
-                url:`${connection.url}/${record.Id}`,
-                notes:null,
-                namespace: record.NamespacePrefix,       
-            }
-    
-            return simplified;          
-        });
-    
-        return wfAlerts;
-    }
+        let soql = {query,filterById:true};
 
-    function parseApexClasses(rawResults){
+        let templateFullName;
 
-        let apexClasses = rawResults.records.map(record => {
-    
-            let simplified = {
-                name:record.MetadataComponentName,
-                type:`ApexClass with EmailTemplate SOQL`,
-                id:record.MetadataComponentId,
-                url:`${connection.url}/${record.MetadataComponentId}`,
-                notes:null,
-                namespace: record.MetadataComponentNamespace,      
-            }
+        //this can throw an error if the email template is in a private user's folder
+        try {
+            let rawResults = await toolingApi.query(soql);
+            templateFullName = rawResults.records[0].FullName;
+            //remove the folder part of the name
+            templateFullName = templateFullName.substr(templateFullName.indexOf('/')+1);
+        } catch (error) {
+            //since we cannot query the email template, we'll make an educated guess
+            //and assume that the full name is the same as the name but with underscores
+            //instead of spaces, which is the default behaviour when you create a template
+            //via the UI and don't manually specificy an API name
+            //i.e "My Template Name" becomes "My_Template_Name"
+            templateFullName = entryPoint.name.replace(/ /g,"_");
+        }
 
-            return simplified;          
-        });
+        query = `SELECT Id, Name , NamespacePrefix FROM 
+        externalString WHERE value IN ('${entryPoint.name}','${entryPoint.id}','${templateFullName}')`
 
-        return apexClasses;
-    }
+        soql =  {query,filterById:false};
 
-    function parseCustomLabels(rawResults){
+        let rawResults = await toolingApi.query(soql);
 
         let customLabels = rawResults.records.map(record => {
     
@@ -97,54 +69,68 @@ async function findReferences(connection,entryPoint){
         return customLabels;
     }
 
-}
+    async function findWorkflowAlerts(){
 
-function createEmailTemplateQuery(emailTemplateId){
+        let ids = utils.filterableId(entryPoint.id);
 
-    let ids = utils.filterableId(emailTemplateId);
+        let query = `SELECT Id,TableEnumOrId,DeveloperName,NamespacePrefix 
+        FROM WorkflowAlert WHERE TemplateId IN ('${ids}')`;
+    
+        //we use a lower version of the API to be able to query the TableEnumOrId
+        //as this field is only available on lower versions
+        let soql = {query,filterById:false,apiVersionOverride:'32.0'};
 
-    let query = `SELECT FullName
-    FROM EmailTemplate WHERE Id IN ('${ids}')`;
+        let rawResults = await toolingApi.query(soql);
 
-    return {query,filterById:true};
-}
+        let wfAlerts = rawResults.records.map(record => {
+    
+            let simplified = {
+                name:`${record.TableEnumOrId}.${record.DeveloperName}`,
+                type:'WorkflowAlert',
+                id: record.Id,
+                url:`${connection.url}/${record.Id}`,
+                notes:null,
+                namespace: record.NamespacePrefix,       
+            }
+    
+            return simplified;          
+        });
+    
+        return wfAlerts;
+    }
 
-function createCustomLabelQuery(entryPoint,templateFullName){
+    /**
+     * Returns apex classes that dynamically reference the emailtemplate object. These classes potentially
+     * use the email template in question. 
+     */
+    async function findApexClasses(){
 
-    //remove the folder part of the name
-    templateFullName = templateFullName.substr(templateFullName.indexOf('/')+1);
+        let query = `select MetadataComponentName, MetadataComponentId,MetadataComponentNamespace from MetadataComponentDependency 
+        where MetadataComponentType = 'ApexClass' AND RefMetadataComponentId = 'EmailTemplate'`;
 
-    let query = `SELECT Id, Name , NamespacePrefix FROM 
-    externalString WHERE value IN ('${entryPoint.name}','${entryPoint.id}','${templateFullName}')`
+        //we use this specific version of the API because on this version the RefMetadataComponentId field
+        //can be filtered by the string 'EmailTemplate'. We want to be able to run this query even if the API
+        //evolves or 'fixes' this
+        let soql = {query,filterById:false,apiVersionOverride:'48.0'};
 
-    return {query,filterById:false};
-}
+        let rawResults = await toolingApi.query(soql);
 
-function createWfAlertQuery(emailTemplateId){
+        let apexClasses = rawResults.records.map(record => {
+    
+            let simplified = {
+                name:record.MetadataComponentName,
+                type:`ApexClass with EmailTemplate SOQL`,
+                id:record.MetadataComponentId,
+                url:`${connection.url}/${record.MetadataComponentId}`,
+                notes:null,
+                namespace: record.MetadataComponentNamespace,      
+            }
 
-    let ids = utils.filterableId(emailTemplateId);
+            return simplified;          
+        });
 
-    let query = `SELECT Id,TableEnumOrId,DeveloperName,NamespacePrefix 
-    FROM WorkflowAlert WHERE TemplateId IN ('${ids}')`;
-
-    return {query,filterById:false,apiVersionOverride:'32.0'};
-
-}
-
-/**
- * Returns apex classes that dynamically reference the emailtemplate object. These classes potentially
- * use the email template in question. 
- */
-function createApexClassQuery(){
-
-    let query = `select MetadataComponentName, MetadataComponentId,MetadataComponentNamespace from MetadataComponentDependency 
-    where MetadataComponentType = 'ApexClass' AND RefMetadataComponentId = 'EmailTemplate'`;
-
-    //we use this specific version of the API because on this version the RefMetadataComponentId field
-    //can be filtered by the string 'EmailTemplate'. We want to be able to run this query even if the API
-    //evolves or 'fixes' this
-    return {query,filterById:false,apiVersionOverride:'48.0'};
-
+        return apexClasses;
+    }
 }
 
 
