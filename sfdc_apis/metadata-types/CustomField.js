dcd0d7731e7ca4fd0d2b4ed2df9eb71563b3cd14
Pablo Gonzalez <pgonzaleznetwork@gmail.com>
2020-11-27 09:29:05 +0000

    added detailed comments (#81)

--- sfdc_apis/metadata-types/CustomField.js
+++ sfdc_apis/metadata-types/CustomField.js
@@ -14,7 +14,7 @@ async function findReferences(connection,entryPoint,cache,options){
     let workflowFieldUpdates = [];
     let metadataTypeRecords = [];
 
-    let entitiyDefinitionFormat = await getEntityDefinitionFormat(restApi,entryPoint.id);
+    let edf = await getEntityDefinitionFormat(restApi,entryPoint.id);
 
     try {
         workflowRules = await findWorkflowRules();
@@ -45,10 +45,21 @@ async function findReferences(connection,entryPoint,cache,options){
     async function findMetadataTypeRecords(){
 
         let metadataTypesUsingField = [];
-
         if(!options.fieldInMetadataTypes) return metadataTypesUsingField;
 
-        let searchValue = `${entitiyDefinitionFormat.entityDefinitionId}.${entitiyDefinitionFormat.shortFieldId}`;
+        function parseMetadataTypeRecord(record){
+
+            let simplified = {
+                name: `${record.DeveloperName} (${record.attributes.type})`,
+                type: 'Custom Metadata Record',
+                id: record.Id,
+                url:`${connection.url}/${record.Id}`,
+                notes:null,       
+            }
+            return simplified;
+        }
+
+        let searchValue = `${edf.entityDefinitionId}.${edf.shortFieldId}`;
 
         let cachedData = cache.getMetadataTypesWithFieldDefinitions();
 
@@ -58,14 +69,7 @@ async function findReferences(connection,entryPoint,cache,options){
 
                 Object.keys(record).forEach(key => {
                     if(typeof record[key] === 'string' && record[key] == searchValue){
-                        let simplified = {
-                            name: `${record.DeveloperName} (${record.attributes.type})`,
-                            type: 'Custom Metadata Record',
-                            id: record.Id,
-                            url:`${connection.url}/${record.Id}`,
-                            notes:null,       
-                        }
-                        metadataTypesUsingField.push(simplified);
+                        metadataTypesUsingField.push(parseMetadataTypeRecord(record));
                     }
                 })
             })   
@@ -73,26 +77,32 @@ async function findReferences(connection,entryPoint,cache,options){
             return metadataTypesUsingField;
         }
 
-    
+        //we need to get all the objects in the org to find
+        //out which ones are actually custom metadata types
         let sObjects = await restApi.getSObjectsDescribe();
         let customMetadataTypes = [];
         
         sObjects.forEach(sobj => {
+            //metadata types end with __mdt
             if(sobj.name.includes('__mdt')){
+                //when using it in queries though, we don't need the __mdt suffix
                 let index = sobj.name.indexOf('__mdt');
                 let name = sobj.name.substring(0,index);
                 customMetadataTypes.push(name);
             }
         });
 
+        //it's possible that the org doesn't have metadata types
+        //so we exit early
         if(customMetadataTypes.length){
 
             let filterNames = utils.filterableId(customMetadataTypes);
 
+            //the sobjects describe call from the rest API that we did earlier doesn't include
+            //the object id, so we need to query it here manually
+            //this will then be used to query all the custom fields that belong to a specific metadata type
             let query = `SELECT Id,DeveloperName FROM CustomObject WHERE DeveloperName  IN ('${filterNames}')`;
-
             let soql = {query,filterById:false,useToolingApi:true};
-
             let rawResults = await restApi.query(soql);
 
             let metadataTypesById = new Map();
@@ -103,6 +113,7 @@ async function findReferences(connection,entryPoint,cache,options){
 
             let filterTableOrEnumIds = utils.filterableId(Array.from(metadataTypesById.keys()));
 
+            //now we query all the custom fields belonging to custom metadata types
             query = `SELECT Id,DeveloperName,TableEnumOrId FROM CustomField WHERE TableEnumOrId  IN ('${filterTableOrEnumIds}')`;
             soql = {query,filterById:false,useToolingApi:true};
 
@@ -110,6 +121,10 @@ async function findReferences(connection,entryPoint,cache,options){
 
             let fullFieldNames = [];
 
+            //once we have all the fields, we build their full name using the metadata type
+            //id map. Ideally we would've queried the full name in the previous query but
+            //the tooling API doesn't allow queries on the fullName if the query returns
+            //more than one result
             rawResults.records.forEach(field => {
                 let metadataTypeName = metadataTypesById.get(field.TableEnumOrId);
                 metadataTypeName += '__mdt';
@@ -117,18 +132,24 @@ async function findReferences(connection,entryPoint,cache,options){
                 fullFieldNames.push(fullFieldName);
             });
 
+            //now that we have the full names, we issue a readMetadata call to inspect the
+            //details of each custom field
             let customFieldsMetadata = await mdapi.readMetadata('CustomField',fullFieldNames);
 
             let fieldsThatReferenceFieldDefinition = [];
 
+            //finally here, we determine if the field is a lookup to a fieldDefinition
             customFieldsMetadata.forEach(fieldMd => {
                 if(fieldMd.referenceTo && fieldMd.referenceTo == 'FieldDefinition'){
                     fieldsThatReferenceFieldDefinition.push(fieldMd.fullName);
                 }
             });
 
-            //now we need to group by custom object
-
+            //now we have the objects and fields that point to a custom field
+            //The next step is to query each individual object, checking if the field in question
+            //matches the search value
+            //to do that, we map the fields by the object name
+            //note that a single metadata type can have multiple fields that point to field definitions
             let fieldsByObjectName = new Map();
 
             fieldsThatReferenceFieldDefinition.forEach(field => {
@@ -146,17 +167,18 @@ async function findReferences(connection,entryPoint,cache,options){
 
             let queries = [];
 
+            //we need to build on query per field because you can't use OR in custom metadata
+            //types SOQL
             for (let [objectName, fields] of fieldsByObjectName) {
      
-                //we need to build on query per field because you can't use OR in custom metadata
-                //types SOQL
                 fields.forEach(field => {
                     let query = `SELECT Id , ${field}, DeveloperName FROM ${objectName} WHERE ${field} != null`;
                     queries.push(query);
                 });
             }
 
-
+            //once we have all the queries, we 
+            //execute them in parallel
             let data = await Promise.all(
 
                 queries.map(async (query) => {
@@ -168,29 +190,27 @@ async function findReferences(connection,entryPoint,cache,options){
             )
 
             let allData = [];
-
             data.forEach(d => allData.push(...d));
 
             cache.cacheMetadataTypesWithFieldDefinitions(allData);
 
             allData.forEach(record => {
 
+                //now we go through the results
+                //if the record has a key, whos value matches the search value, we 
+                //consider this a match
+                //we do this because as explained earlier, a single record can have multiple
+                //fields of type field definition. So rather than keeping track of all the
+                //fields per object, we just check if a key value matches the search value
                 Object.keys(record).forEach(key => {
                     if(typeof record[key] === 'string' && record[key] == searchValue){
-                        let simplified = {
-                            name: `${record.DeveloperName} (${record.attributes.type})`,
-                            type: 'Custom Metadata Record',
-                            id: record.Id,
-                            url:`${connection.url}/${record.Id}`,
-                            notes:null,       
-                        }
-                        metadataTypesUsingField.push(simplified);
+                        metadataTypesUsingField.push(parseMetadataTypeRecord(record));
                     }
                 })
             })            
         }
         return metadataTypesUsingField;
-    
+
     }
 
     async function findWorkflowFieldUpdates(){
@@ -202,7 +222,7 @@ async function findReferences(connection,entryPoint,cache,options){
         //for standard objects, this would be 'Account.00N3h00000DAO0J'
         //the 2nd id is the 15 digit version of the custom field id
 
-        let fieldDefinitionId = utils.filterableId(`${entitiyDefinitionFormat.entityDefinitionId}.${entitiyDefinitionFormat.shortFieldId}`);
+        let fieldDefinitionId = utils.filterableId(`${edf.entityDefinitionId}.${edf.shortFieldId}`);
 
         let query = `SELECT Id,name FROM WorkflowFieldUpdate WHERE FieldDefinitionId IN ('${fieldDefinitionId}')`;
         let soql = {query,filterById:true,useToolingApi:true};
@@ -318,8 +338,7 @@ async function findReferences(connection,entryPoint,cache,options){
                         }
                     }
                 }
-    
-                
+            
                 if(wf.criteriaItems){
     
                     if(Array.isArray(wf.criteriaItems)){
@@ -374,5 +393,6 @@ async function getEntityDefinitionFormat(restApi,id){
 
     return {entityDefinitionId,shortFieldId}
 }
+
 
 module.exports = findReferences;
