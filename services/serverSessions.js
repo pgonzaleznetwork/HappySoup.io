a2db70b3c191c01f12b65420d622e3cc91fbe938
pablo <pgonzaleznetwork@gmail.com>
2021-03-31 19:41:46 +0100

    job id is validated before connecting to redis

--- services/serverSessions.js
+++ services/serverSessions.js
@@ -21,10 +21,41 @@ async function validateSessions(req,res,next){
                 throw new ErrorHandler(404,'session-expired','server session was alive but sfdc token was expired');
             }  
         }
-        } catch (error) {
-            next(error);
-        }
+    } catch (error) {
+        next(error);
+    }
 }
+
+/**
+ * We make sure that the job id matches the identity info (org id + user id) of the logged in user. This prevents
+ * users from being able to access job information for jobs submitted by other orgs.
+ */
+async function validateJobId(req,res,next){
+
+    try {
+
+        //job id format 00D3h000005XLUwEAO.0053h000002JF4cAAG:usage-00N3h00000DdZSIEA3-CustomField1617213923756
+        let jobId = req.params.id;
+        let identityKeyFromJob = jobId.split(':')[0];
+
+        let identityKey = getIdentityKey(req);
+
+        if(identityKeyFromJob != identityKey){
+            throw new ErrorHandler(404,'invalid-job-for-session','The job id does not match the server side session');
+        }
+        else{
+            next();
+        }
+    } catch (error) {
+        next(error); 
+    }
+
+}
+
+function getIdentityKey(req){
+    return `${req.session.identity.orgId}.${req.session.identity.userId}`;
+}
+
 
 /**
  * Quick ping to salesforce to see if the oauth token is still alive
@@ -65,5 +96,7 @@ async function logout(session){
 module.exports = {
     validateSessions,
     getConnection,
-    logout
+    logout,
+    getIdentityKey,
+    validateJobId
 }
