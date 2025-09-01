729b193c0d1536546c1554e1036bb5d705ee973b
Pablo Gonzalez <pgonzaleznetwork@gmail.com>
2025-09-01 16:31:55 +0100

    survey

--- frontend/src/views/login/components/Info.vue
+++ frontend/src/views/login/components/Info.vue
@@ -1,11 +1,16 @@
 <template>
    <div class="info">
-
-   <a href="https://www.learn-apex.com/onboarding/email" target="_blank" >
-        <img src="../../../assets/face-learn-apex.com.png" />
-    </a>
-
+    <div class="form-container">
+      <iframe 
+        src="https://docs.google.com/forms/d/e/1FAIpQLSeJwZImy7LFQ3kttmJ6kKAXdtH-nz1EEff19zjmNlqYTVGqjQ/viewform?embedded=true" 
+        height="2847" 
+        frameborder="0" 
+        marginheight="0" 
+        marginwidth="0">
+        Loading…
+      </iframe>
     </div>
+   </div>
 </template>
 
 <script>
@@ -16,25 +21,40 @@ export default {
 
 <style lang="scss" scoped>
 
+    .info {
+        height: 100%;
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding: 10px;
+    }
+
+    .form-container {
+        width: 100%;
+        height: 85vh;
+        background: white;
+        border-radius: 8px;
+        box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+        overflow: hidden;
+    }
+
+    iframe {
+        width: 100%;
+        height: 100%;
+        border: none;
+    }
+
     .ad{
         text-align: center;
-
-        
     }
 
     .has-text-info{
         color:$alternative-background !important;
     }
 
-   
-
     p{
         margin-bottom: 15px;
-    }
-
-    iframe{
-        width: 160;
-        height: 115;
     }
 
     .pure-center{
@@ -44,7 +64,5 @@ export default {
     .fa-youtube{
         color:rgb(232, 120, 120);
     }
-
-    
 
 </style>
